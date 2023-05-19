b723d6e408b7f50936a07ba93bdc442d358eed60
ParasSingh1510 <133941645+ParasSingh1510@users.noreply.github.com>
2023-05-19 09:42:41 +0530

    modified C3

--- src/Classess/C3.java
+++ src/Classess/C3.java
@@ -2,7 +2,8 @@ package Classess;
 
 public class C3 {
 	public static void main(String[] args) {
-		System.out.println("From C3");
+		System.out.println("From C3-New Data");
+		System.out.println("New code -> Fresh Code");
 	}
 
 }
